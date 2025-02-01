e2fb37c736ed54c193750e5bf9103ba2695991f1
Nil Xandrea Montejo <montejo.nilxandrea@gmail.com>
2025-02-01 19:09:55 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let firstName= "Nil Xandrea";
+let age= 19;
+let isStudent= true;
+
+document.getElementById("p1").textContent = `your name is ${firstName}`;
+document.getElementById("p2").textContent = `you are ${age} years old`;
+document.getElementById("p3").textContent = isStudent;
